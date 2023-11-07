6db8d0cb9c9706b8436591da7c1ba805223a39f9
Minngc <ming109152@gmail.com>
2023-11-07 16:10:12 +0800

    pages

--- src/main/resources/static/js/searchUserPanel.js
+++ src/main/resources/static/js/searchUserPanel.js
@@ -3,12 +3,12 @@ const cookie = document.cookie
 if(cookie.startsWith("username=")) {
     console.log(123)
     document.getElementsByClassName("usr-panel")[0].style.display = "none";
-    document.getElementsByClassName("usr-msg")[0].style.display="block";
+    document.getElementsByClassName("usr-msg")[0].style.display="flex";
     localStorage.setItem("username", cookie.split(";")[0].split("=")[1])
     document.getElementById("username").innerHTML = localStorage.getItem("username")
 }
 else if(localStorage.getItem("username")) {
     document.getElementsByClassName("usr-panel")[0].style.display = "none";
-    document.getElementsByClassName("usr-msg")[0].style.display="block";
+    document.getElementsByClassName("usr-msg")[0].style.display="flex";
     document.getElementById("username").innerHTML = localStorage.getItem("username")
 }
